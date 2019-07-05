b965f9b9fa5ad0737233c40e73ecec12e8624392
VictorGOrellana <vitor_orellana@hotmail.com>
2019-07-05 13:04:24 -0300

    victor

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,20 @@
+// comentario de uma linha utiliza o " barra  barra "
+
+
+
+/* 
+    comentario 
+    de
+    multiplas
+    linhas 
+*/
+
+
+/*
+* Comentarios
+* de multiplas 
+* linhas 
+*/ 
+
+
+
